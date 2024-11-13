299349b4fe10a365c130fa49d526e6e214d73541
IrfanAnsarii <irfangittech@gmail.com>
2024-11-13 07:02:34 +0530

    finished up[dating and deleting post with images

--- posts/update.php
+++ posts/update.php
@@ -17,25 +17,41 @@ if ( isset( $_GET[ 'upd_id' ] ) ) {
         if ( $_POST[ 'title' ] == '' or $_POST[ 'subtitle' ] == '' or $_POST[ 'body' ] == ''){
             echo 'one input or more are empty';
         } else {
+
+
+          unlink("images/" . $rows->img. "");
+
+
             $title = $_POST[ 'title' ];
             $subtitle = $_POST[ 'subtitle' ];
             $body = $_POST[ 'body' ];
+            $img = $_FILES[ 'img' ][ 'name' ];
+            $dir = 'images/' . basename( $img );
 
-            $update = $conn->prepare( "UPDATE posts SET title = :title, subtitle=:subtitle, body=:body WHERE id = '$id' " );
+            $update = $conn->prepare( "UPDATE posts SET title = :title, subtitle=:subtitle, 
+            body=:body, img = :img WHERE id = '$id' " );
 
             $update->execute( [
                 'title'=>$title,
                 'subtitle'=>$subtitle,
-                'body'=>$body
+                'body'=>$body,
+                'img'=>$img
+
             ] );
-            header( 'location: http://localhost/blog/index.php' );
+            
+            if ( move_uploaded_file( $_FILES[ 'img' ][ 'tmp_name' ], $dir ) ) {
+                    header( 'location: http://localhost/blog/index.php' );
+                }
+            
+            
+            //header( 'location: http://localhost/blog/index.php' );
         }
     }
 }
 
 ?>
 
-<form method = 'POST' action = "update.php?upd_id= <?php echo $id; ?>">
+<form method = 'POST' action = "update.php?upd_id= <?php echo $id; ?>" enctype="multipart/form-data" >
 <!-- Email input -->
 <div class = 'form-outline mb-4'>
 <input type = 'text' name = 'title' value = "<?php echo $rows->title; ?>" id = 'form2Example1' class = 'form-control' placeholder = 'title' />
@@ -43,13 +59,15 @@ if ( isset( $_GET[ 'upd_id' ] ) ) {
 </div>
 
 <div class = 'form-outline mb-4'>
-<input type = 'text' name = 'subtitle' value = "<?php echo $rows->title; ?>"  id = 'form2Example1' class = 'form-control' placeholder = 'subtitle' />
+<input type = 'text' name = 'subtitle' value = "<?php echo $rows->subtitle; ?>"  id = 'form2Example1' class = 'form-control' placeholder = 'subtitle' />
 </div>
 
 <div class = 'form-outline mb-4'>
 <textarea type = 'text' name = 'body'  id = 'form2Example1' class = 'form-control' placeholder = 'body' > <?php echo $rows->body;
 ?></textarea>
 </div>
+
+<?php echo "<img src='images/".$rows->img."' width='400' height='200' />"; ?>
 
 <div class = 'form-outline mb-4'>
 <input type = 'file' name = 'img' id = 'form2Example1' class = 'form-control' placeholder = 'image' />
